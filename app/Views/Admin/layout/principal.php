3dd1645d6a4e0fde2697b92445ee3df6377e5aaa
Vinicius Hermes <viniciushermes13@gmail.com>
2024-08-01 21:52:58 -0300

    update

--- app/Views/Admin/layout/principal.php
+++ app/Views/Admin/layout/principal.php
@@ -285,7 +285,7 @@
                     <div class="d-sm-flex justify-content-center justify-content-sm-between">
                         <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright ©
                             Food Delivery</span>
-                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> VINICIUS ALMEIDA HERMES</span>
+                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> VINÍCIUS ALMEIDA HERMES</span>
                     </div>
                 </footer>
                 <!-- partial -->
