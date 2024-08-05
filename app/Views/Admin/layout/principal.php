5004fd4ce60d0ce4a9e7c42c0d3905c60e9cf80d
Vinicius Hermes <viniciushermes13@gmail.com>
2024-08-05 09:07:04 -0300

    demo

--- app/Views/Admin/layout/principal.php
+++ app/Views/Admin/layout/principal.php
@@ -34,8 +34,8 @@
                 <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
 
                     <a class="navbar-brand brand-logo" href="index.html"><img src="image" alt="Placeholder" /></a>
-                    <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/logo-mini.svg"
-                            alt="logo" /></a>
+                    <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/"
+                            alt="Placeholder" /></a>
                     <button class="navbar-toggler navbar-toggler align-self-center" type="button"
                         data-toggle="minimize">
                         <span class="mdi mdi-sort-variant"></span>
